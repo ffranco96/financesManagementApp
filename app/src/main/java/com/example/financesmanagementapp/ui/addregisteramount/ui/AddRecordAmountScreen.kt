a87f70bb7e94df54cb7720ae97d495c38496a7d7
Franco Macen <macen38787@estudiantes.untref.edu.ar>
2026-05-04 23:11:41 -0300

    fix: solved problem of cursor, now is fixed at the end of the TextField.

--- app/src/main/java/com/example/financesmanagementapp/ui/addregisteramount/ui/AddRecordAmountScreen.kt
+++ app/src/main/java/com/example/financesmanagementapp/ui/addregisteramount/ui/AddRecordAmountScreen.kt
@@ -31,14 +31,20 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -133,6 +139,16 @@ fun BodyContent(
     currencyList: List<String>,
     innerPadding: PaddingValues
 ) {
+    // Synchonized with the view model valueAmountText
+    var textFieldValue by remember(valueAmountText) {
+        mutableStateOf(
+            TextFieldValue(
+                text = valueAmountText,
+                selection = TextRange(valueAmountText.length)
+            )
+        )
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -144,8 +160,12 @@ fun BodyContent(
         Text("Ingrese monto", fontSize = 42.sp, modifier = Modifier.align(Alignment.CenterHorizontally))
 
         TextField(
-            value = valueAmountText,
-            onValueChange = onAmountTextChange, // Kotlin allows simplification because parameter and onValueChange has similar sign
+            value = textFieldValue,
+            onValueChange = { newValue ->
+                val forcedCursorToEnd = newValue.copy(selection = TextRange(newValue.text.length))
+                textFieldValue = forcedCursorToEnd
+                onAmountTextChange(forcedCursorToEnd.text)
+            },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             singleLine = true,
             modifier = Modifier.fillMaxWidth(),
@@ -153,6 +173,11 @@ fun BodyContent(
                 textAlign = TextAlign.End,
                 fontSize = 55.sp
             ),
+            colors = TextFieldDefaults.colors(
+                cursorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent, // Opcional: oculta la línea inferior al enfocar
+                unfocusedIndicatorColor = Color.Transparent
+            )
         )
 
         Spacer(Modifier.height(20.dp))
